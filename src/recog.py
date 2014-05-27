ffd4a0f387399d0d390341d29fbaf62e8b2581d9
tb <tb@pedromoreira.org>
2014-05-28 00:33:36 +0100

    caminho certo?

--- src/recog.py
+++ src/recog.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
-import scipy
+import scipy.signal as signal
 
 class Recog:
+    # kirsch compass masks
     masks = [
         np.array([[-3, -3, 5], [-3, 0, 5], [-3, -3, 5]]),
         np.array([[-3, 5, 5], [-3, 0, 5], [-3, -3, -3]]),
@@ -19,22 +20,33 @@ class Recog:
     def __init__(self, image):
         
         self.image = cv2.imread(image, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        # criar convultions da imagem para cada uma das masks
+        # - assim ou cv2.filter2D ?
+        self.convultions = [signal.convolve2d(self.image, self.masks[x]) for x in range(len(self.masks))]
+        
         ldn = [[0 for x in range(len(self.image))] for x in range(len(self.image[0]))]
-        for x in range(len(image)):
-            for y in range(len(image[0])):
+        for x in range(len(self.image)):
+            for y in range(len(self.image[0])):
                 ldn[x][y] = self.LDN(x,y)
-        print np.amax(ldn), np.amin(ldn)
+    
+        # passo seguinte: 
+        # - dividir a imagem em 25 partes para calcular os histogramas ?
+        # - ou o cálculo dos ldn deveriam ter sido já feitos nas subdivisoes da imagem?
+        x = 0
+        for i in self.convultions:
+            cv2.imshow('img%d' % x, i)
+            x+=1
+        
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        
+
 
     def LDN(self, x, y):
-        i, j = self.kirschConv(x, y)
-        return 8 * i + j
-
-    def kirschConv(self, x, y, i = None, j = None, k = len(masks)):
-        if k == 0 : return (i, j)
-        k-=1
-        conv = cv2.filter2D(self.image, -1, self.masks[k], (x, y))
-        if i == None: i = np.amax(conv)
-        if j == None: j = np.amin(conv)
-        return self.kirschConv(x, y, max(i, np.amax(conv)), min(i, np.amin(conv)), k)
+        i = 8 * max(self.convultions[k][x][y] for k in range(len(self.masks)))
+        j = min(self.convultions[k][x][y] for k in range(len(self.masks)))
+        
+        return i + j
         
 r = Recog("mirror.jpg")
