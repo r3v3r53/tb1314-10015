7e8b0fab117290705cb7eea066b05eb7524e9195
tb <tb@pedromoreira.org>
2014-05-30 10:11:35 +0100

    compare recursivo

--- src/main.py
+++ src/main.py
@@ -1,17 +1,12 @@
 from LH import LH
 
-def compare(f1, f2):
-    result = 0.0
-    temp = None
-    for i in range(len(f1)):
-        if f1[i] + f2[i] != 0:
-            if temp == None:
-                temp = ((f1[i] - f2[i])**2 / (f1[i] + f2[i]))
-            else:
-                temp = temp + ((f1[i] - f2[i])**2 / (f1[i] + f2[i]))
-       
-#        result += ((f1[i] - f2[i])**2)/(f1[i] + f2[i])
-    return temp
+def compare(f1, f2, i = None, val = 0):
+    if i == None: i = len(f1)
+    if i == 0: return val
+    i -= 1
+    if f1[i] + f2[i] != 0:
+        val += ((f1[i] - f2[i])**2 / (f1[i] + f2[i]))
+    return compare(f1, f2, i, val)
 
 
 uma = LH("mirror.jpg")
@@ -24,6 +19,4 @@ f1_f2 = compare(f1,f2)
 f1_f3 = compare(f1,f3)
 
 print f1_f2, f1_f3
-#(f1 -f2)^2 / f1+f2
 
-#print a
