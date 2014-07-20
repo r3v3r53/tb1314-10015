f55c83fbf90948ed175e2a686e691217c92609d7
tb <tb@pedromoreira.org>
2014-07-20 09:36:47 +0100

    reordenacao dos ficheiros finais, fantando ainda terminar o Gaussian

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from code import *
+from Kirsh import *
+
 def compare(f1, f2, i = None, val = 0):
     if i == None: i = len(f1)
     if i == 0: return val
@@ -14,62 +15,12 @@ print
 uma = Kirsh("mirror.jpg")
 duas = Kirsh("mirror2.jpg")
 tres = Kirsh("people.jpg")
-#f1 = uma.code()
-#f2 = duas.code()
-#f3 = tres.code()
-#f1_f2 = compare(f1,f2)
-#print "f1 - f2: %s" % f1_f2
-#f1_f3 = compare(f1,f3)
-#print "f1 - f3: %s" % f1_f3
-#f2_f3 = compare(f2,f3)
-#print "f2 - f3: %s" % f2_f3
-#f1_f1 = compare(f1,f1)
-#print "f1 - f1: %s" % f1_f1
-#f1_f1 = compare(f1,f1)
-#print "f1 - f1: %s" % f1_f1
-#f3_f3 = compare(f3,f3)
-#print "f3 - f3: %s" % f3_f3
-#f2_f2 = compare(f2,f2)
-#print "f2 - f2: %s" % f2_f2
-#a = f1
-#print "A"
-#print a
-
-print
-print "G A U S S I A N"
-print
-uma = Gaussian("mirror.jpg", 0.9)
-duas = Gaussian("mirror.jpg", 0.9)
-tres = Gaussian("people.jpg", 0.9)
-#f1 = uma.code()
-#f2 = duas.code()
-#b = f1
-#c = f2
-#f2 = duas.code()
-#f3 = tres.code()
-#f1_f2 = compare(f1,f2)
-#print "f1 - f2: %s" % f1_f2
-#f1_f3 = compare(f1,f3)
-#print "f1 - f3: %s" % f1_f3
-#f2_f3 = compare(f2,f3)
-#print "f2 - f3: %s" % f2_f3
-#f1_f1 = compare(f1,f1)
-#print "f1 - f1: %s" % f1_f1
-#f1_f1 = compare(f1,f1)
-#print "f1 - f1: %s" % f1_f1
-#f3_f3 = compare(f3,f3)
-#print "f3 - f3: %s" % f3_f3
-#f2_f2 = compare(f2,f2)
-#print "f2 - f2: %s" % f2_f2
-
-
-print "B"
-print b
-print "C"
-print c
-#f1 = uma.LDNgCode()
-#f2 = duas.LDNgCode()
-#f3 = tres.LDNgCode()
-#f1__f2 = compare(f1,f2)
-#f1__f3 = compare(f1,f3)
-
+f1 = uma.code()
+f2 = duas.code()
+f3 = tres.code()
+f1_f2 = compare(f1,f2)
+print "f1 - f2: %s" % f1_f2
+f1_f3 = compare(f1,f3)
+print "f1 - f3: %s" % f1_f3
+f2_f3 = compare(f2,f3)
+print "f2 - f3: %s" % f2_f3
